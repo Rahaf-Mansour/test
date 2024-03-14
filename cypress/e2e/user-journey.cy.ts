1cc1692a3c6356ee23775c112dedb29f9f45e7d2
Rahaf <rahafmansour2018@gmail.com>
2024-03-14 22:57:25 +0200

    add my tests and finishing the first course (Testing Your First Application)

---
+++ cypress/e2e/user-journey.cy.ts
@@ -0,0 +1,30 @@
+describe("User Journey", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/")
+  })
+
+  it("a user can find a course on the home page and complete all courses lessons", () => {
+    cy.getByData("course-0").find("a").contains("Get started").click()
+    cy.location("pathname").should("equal", "/testing-your-first-application")
+    cy.getByData("next-lesson-button").click()
+    cy.location("pathname").should(
+      "equal",
+      "/testing-your-first-application/app-install-and-overview"
+    )
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").should("exist").click()
+    cy.location("pathname").should(
+      "eq",
+      "/testing-your-first-application/installing-cypress-and-writing-our-first-test"
+    )
+    cy.getByData("challenge-answer-0").should("exist").click()
+    cy.getByData("next-lesson-button").click()
+    cy.location("pathname").should(
+      "eq",
+      "/testing-your-first-application/setting-up-data-before-each-test"
+    )
+    cy.getByData("challenge-answer-0").should("exist").click()
+    cy.getByData("next-lesson-button").click()
+    cy.location("pathname").should("eq", "/")
+  })
+})
